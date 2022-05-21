df0596c49a86d6d8cddfb9c70706f29db087cca7
Artur Pieńkowski <artpienkowski@gmail.com>
2022-05-21 17:06:18 +0200

    amend code for variables for computerMove

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
-}
-if (randomNumber == '2') {
-  computerMove = 'papier';
-}
-if (randomNumber == '3') {
-  computerMove = 'nożyce';
+} else {
+  if (randomNumber == '2') {
+    computerMove = 'papier';
+  } else {
+    if (randomNumber == '3') {
+      computerMove = 'nożyce';
+    }
+  }
 }
 printMessage('Mój ruch: ' + computerMove);
